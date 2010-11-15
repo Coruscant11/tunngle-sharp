23561f6953ada4c9754e278a4fe9024a9e03528b
jdluzen <jdluzen@localhost>
2010-11-14 23:22:47 -0500

    Initial commit.

---
+++ tunngle-sharp/Interop.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace tunngle_sharp
+{
+    public class Interop
+    {
+        public const string DllName = "Launcher.dll";
+        
+        [DllImport(DllName, CallingConvention = CallingConvention.StdCall)]
+        public static extern int Init(string company, string project);
+
+
+    }
+}
